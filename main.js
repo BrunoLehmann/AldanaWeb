1c80a92c89558012630f12a5c6be22438872188b
BrunoLehmann <brunolehmann123@gmail.com>
2024-01-09 14:33:55 -0300

    a
    s

--- main.js
+++ main.js
@@ -5,14 +5,24 @@ form.addEventListener('submit', e => {
     e.preventDefault()
     const data = new FormData(form);
     var output = "";
-    var scriptURL = 'https://script.google.com/macros/s/AKfycbyi__5s9lxxFVJAfXG8I85LymIMtan9f-NaKDEgT1z6sLD1UEcfP2ku9oBNjFrFrTDVmQ/exec'
-    
+        
     for(const entry of data){
         output = `${output}${entry[0]}=${entry[1]}\r`;
         console.log(entry[1]);
     }
     
-    document.querySelector("#datos").innerHTML = output;
-    fetch(scriptURL, {method : 'POST', body: data})
-       
+    fetch("datos.html", {
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            // Manejar la respuesta del servidor
+            if (!response.ok) {
+                throw new Error('Error al enviar datos al servidor');
+            }
+        })
+        .catch(error => {
+            // Manejar el error
+            console.error("Error al enviar datos al servidor:", error);
+        });
 });
